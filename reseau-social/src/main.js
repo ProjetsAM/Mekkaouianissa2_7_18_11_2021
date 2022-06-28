72dcfed5b5f2031cf2a7cb95cb5cf9b8364693db
AnissaMEKKAOUI <adraris@gmail.com>
2022-06-28 09:39:03 +0200

    modifs

--- reseau-social/src/main.js
+++ reseau-social/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import des fichiers CSS des frameworks [Bootstrap] et [BootstrapVue] 
 import './plugins/bootstrap-vue'
 
 
